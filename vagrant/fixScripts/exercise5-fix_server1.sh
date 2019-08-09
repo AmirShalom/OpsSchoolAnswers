a09cbcbf7b4bafc0d287b2266ac9f30c0e24c816
Shalom, Amir <Amir.Shalom@verint.com>
2019-08-09 18:17:12 +0300

    answering all questions

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+ssh-keygen -N "" -f /root/.ssh/id_rsa
+cp /root/.ssh/id_rsa.pub /vagrant/server1_root.pub
+
+ssh-keygen -N "" -f /home/vagrant/.ssh/id_rsa
+chown -R vagrant:vagrant /home/vagrant/.ssh/
+cp /home/vagrant/.ssh/id_rsa.pub /vagrant/server1_vagrant.pub
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+
+cat /vagrant/server1_root.pub >> /home/vagrant/.ssh/authorized_keys
+cat /vagrant/server1_vagrant.pub >> /home/vagrant/.ssh/authorized_keys
